090384dc3f9b0fd158989f9036fae589702c1580
daniMartinez <danielmartinez@ipglobal.es>
2024-02-12 18:27:57 +0100

    Initial Commit

---
+++ JiraTimeTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JiraTimeTracker
+//
+//  Created by Dani Martínez on 12/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JiraTimeTracker/JiraTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiraTimeTrackerApp.swift
+//  JiraTimeTracker
+//
+//  Created by Dani Martínez on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JiraTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
